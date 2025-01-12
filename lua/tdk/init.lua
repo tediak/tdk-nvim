c0ac1613bf1b9d45e86eb40cbd8bf9dd7254a853
Ivan Yadzvinsky <ivan.yadzvinsky@gmail.com>
2025-01-12 17:20:06 +0200

    update colorscheme and snacks

--- lua/tdk/init.lua
+++ lua/tdk/init.lua
@@ -1,28 +1,8 @@
 require("tdk.core")
 require("tdk.lazy")
 
--- vim.cmd.colorscheme("rose-pine-moon")
--- vim.cmd.colorscheme("kanagawa")
--- vim.cmd.colorscheme("synthwave84")
--- vim.cmd.colorscheme("tokyonight-night") -- available themes: night, storm, moon, day
--- vim.cmd.colorscheme("everforest")
 vim.cmd.colorscheme("gruvbox")
--- vim.cmd.colorscheme("tundra")
--- vim.cmd.colorscheme("poimandres")
--- vim.cmd.colorscheme("catppuccin-macchiato")
--- vim.cmd("colorscheme github_dark")
--- vim.cmd('colorscheme ayu-dark')
--- vim.cmd('colorscheme ayu-dark')
 
--- vim.g.moonflyTransparent = true
--- vim.g.moonflyTerminalColors = false
--- vim.cmd("colorscheme moonfly")
-
--- Transparent bg for line number column
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-
-vim.opt.conceallevel = 2
 vim.opt.concealcursor = "nc"
 
 -- Diagnostics
--- lua/tdk/plugins/colorschemes/gruvbox.lua
+++ lua/tdk/plugins/colorschemes/gruvbox.lua
@@ -14,7 +14,6 @@ return {
     },
     transparent_mode = true,
     overrides = {
-      BlinkCmpMenuBorder = { bg = "#FE8018", fg = "#FE8018" },
       BlinkCmpMenu = { link = "NormalFloat" },
       BlinkCmpScrollbarThumb = { link = "NormalFloat" },
       BlinkCmpScrollbarGutter = { link = "NormalFloat" }
